7592abda34fe234f8e39bb1c4cfc87edd2d8f926
Yisehak Gebeyehu <yisehakgebeyehu34@gmail.com>
2024-07-09 11:45:40 +0300

    Header and Footer

---
+++ Netflix-Clone/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../Componets/Header/Header";
+import Footer from "../../Componets/Footer/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
